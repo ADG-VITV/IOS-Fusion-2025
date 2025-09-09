e1408fe1f8805ac4a3877ea11d54a88c58c50a36
Syed GM Ibrahim <syedgibrahim@gmail.com>
2025-09-09 10:22:03 +0530

    Timeline and fonts added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 // import LandingPageMobile from "@/sections/Home-mobile";
 import Description from "@/sections/Description";
 // import Speakers from "@/sections/Speakers";
-// import Timeline from "@/sections/Timeline";
+import Timeline from "@/sections/Timeline";
 // import Partners from "@/sections/Partners";
 // import FAQ from "@/sections/FAQ";
 // import Footer from "@/sections/Footer";
@@ -20,9 +20,9 @@ export default function Home() {
         <LandingPageMobile />
       </div> */}
       <Description />
-      {/* <Speakers />
+      {/* <Speakers /> */}
       <Timeline />
-      <Partners />
+      {/* <Partners />
       <FAQ />
       <Footer />
       <ScrollTop /> */}
---
+++ src/components/Event.tsx
@@ -0,0 +1,24 @@
+interface EventProps {
+  className?: string;
+  onClick?: () => void;
+  style?: React.CSSProperties;
+  event: string,
+  time: string, 
+  venue: string
+}
+
+export default function Event({ className, onClick, style, event, time, venue}: EventProps) {
+  return (
+    <div
+      onClick={onClick}
+      className={`rounded-xl lg:w-[23rem] text-white px-3 py-2 lg:px-5 lg:py-3 font-bold ${className}`}
+      style={{
+        transition: "background-color 0.3s ease-in-out", 
+      }}
+    >
+      <h1 className="text-sm sm:text-base">{event}</h1>
+      <p className="text-xs text-[#9f82f1]">{time}</p>
+      <p className="text-xs text-gray-400">{venue}</p>
+    </div>
+  );
+}
